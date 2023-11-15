ceaca11268637821d6dd77c12b6136ddfde69b57
Srinivasarao Maddila <maddila.srinivasa@cloudeq.com>
2023-11-15 14:29:14 +0530

    updated the workflow

--- s3.tf
+++ s3.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "my-unique-bucket"  # Set a unique name for your bucket
 
-  acl    = "private"  # Access Control List, can be "private", "public-read", "public-read-write", "authenticated-read", etc.
+  acl    = "public"  # Access Control List, can be "private", "public-read", "public-read-write", "authenticated-read", etc.
 
   versioning {
     enabled = true  # Enable versioning for the bucket
